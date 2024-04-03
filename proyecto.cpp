f9fcf5bbf82a986fbfd10eabd3ec36b95e79e3f3
DJavierSolis <126303421+DJavierSolis@users.noreply.github.com>
2024-04-03 15:56:55 -0600

    agregar negaciones
    Agregue la negación y doble negación.
     cambio de estilo de menu pricipal usando void()

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,25 +1,65 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
-int main (){
-int menu_opc, num_prepo, condicion,operador;
-  cout <<"========================\n OPERADORES LOGICOS \n========================\n";
-cout <<"Cuantas condiciones habra. \n 1 | Una preposicion. (p) \n 2 | Dos preposiciones.  \n 3 | Mas de dos preposiciones. \n";
-cin>>menu_opc;
-system("cls");
-switch (menu_opc){
-case 1:
- cout <<"========================\n Una preposicion \n========================\n";
+
+//menu principal
+void menu(){
+cout <<"========================\n OPERADORES LOGICOS \n========================\n";
+cout <<"Cuantas condiciones habra. \n 1 | Una preposicion.\n 2 | Dos preposiciones.  \n 3 | Mas de dos preposiciones. \n";
+}
+//del primer switch opcion 1 una preposicion
+void Preposicion1(){
 cout <<"Que operador logico sera. \n 1 | Negacion ~ \n 2 | Doble negacion ~~\n";
+}
+//condicion de una preposicion de negacion y doble negacion
+void negaciones(){
+    int valor, operador;
 cin>>operador;
 system("cls");
-cout <<operador;
+//SWITCH 1.1 negacion
+switch (operador){
+case 1:
+cout <<"========================\n Negacion ~ \n========================\n";
+cout <<" ingrese el valor de la proposicion. \n 1 | verdadero \n 0 | Falsa \n";
+cin >>valor;
+if (valor == 1 ){
+cout <<"~P Falso \n"; 
+}else if (valor ==0){
+    cout <<" ~P Verdadero \n"; 
+}  getch();
     break;
 case 2:
-cout <<"========================\n Una preposicion 2 \n========================\n";
+cout <<"========================\n Doble Negacion ~~ \n========================\n";
+cout <<" ingrese el valor de la proposicion. \n 1 | verdadero \n 0 | Falsa \n";
+cin >>valor;
+if (valor == 0 ){
+cout <<"~(~P) Falso \n"; 
+}else if (valor == 1 ){
+    cout <<" ~(~P) Verdadero \n"; 
+} getch();
+    break;
+default:
+    break;
+}
+}
+//int principal 
+int main (){
+int menu_opc;
+menu();
+cin>>menu_opc;
+system("cls");
+//PRIMER SWITCH
+switch (menu_opc){
+case 1:
+cout <<"========================\n Una preposicion \n========================\n";
+Preposicion1();
+negaciones();
+    break;
+case 2:
+cout <<"========================\n Dos preposiciones \n========================\n";
     break;
 case 3:
-cout <<"========================\n Una preposicion 3 \n========================\n";
+cout <<"========================\n Mas preposiciones \n========================\n";
     break;
 default:
   getch();
